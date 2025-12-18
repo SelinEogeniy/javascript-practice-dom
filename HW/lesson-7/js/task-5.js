8b8f4204b92e42de7b511836504dc17895707aa0
SelinJax <seljax92@gmail.com>
2025-12-18 22:12:30 +0200

    -newTask

--- HW/lesson-7/js/task-5.js
+++ HW/lesson-7/js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refsHw5 = {
+  colorName: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+};
+
+const onChangeColorBtnClick = () => {
+  let random = getRandomHexColor();
+  refsHw5.body.style.backgroundColor = random;
+  refsHw5.colorName.textContent = random;
+};
+
+refsHw5.changeColorBtn.addEventListener("click", onChangeColorBtnClick);
